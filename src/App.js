e3da1b68070dcc1b416a53375e9f968a9a448185
quangvuminh2000 <quang.vuminh@hcmut.edu.vn>
2022-06-15 22:04:28 +0700

    Add API from ExerraPhishing to detect phishing website

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import { Box, TextField } from '@mui/material';
+import React, { useState } from 'react';
 import './Assets/Styles/App.css';
+import { ExerraPhishing } from './Middlewares';
 
 
 const App = () => {
+	const [domain, setDomain] = useState('google.com');
 	return (
 		<>
 			<p className="message">Hello World!</p>
+			<Box
+				component='form'
+				noValidate
+				autoComplete='off'
+				alignItems='center'
+				justifyContent='center'
+				display='flex'
+				minWidth='100vw'
+				marginBottom='3vh'
+			>
+				<TextField
+					required
+					id='outline-required'
+					label='Required'
+					defaultValue={domain}
+					onChange={(e) => setDomain(e.target.value)}
+					margin='dense'
+					color='secondary'
+				/>
+			</Box>
+			<ExerraPhishing domain={domain}/>
 		</>
 	);
 };
---
+++ src/Middlewares/get-apis.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+
+const ExerraPhishing = ({domain}) => {
+	const [isLoading, setLoading] = useState(true);
+	const [data, setData] = useState(false);
+
+	const getFishingScam = async ({domain}) => {
+		try {
+			const options = {
+				method: 'GET',
+				headers: {
+					'X-RapidAPI-Key': '685d2c8bedmsh3f533f17cc364afp19d020jsn5cae9ed05d0b',
+					'X-RapidAPI-Host': 'exerra-phishing-check.p.rapidapi.com'
+				}
+			};
+
+			const response = await fetch(`https://exerra-phishing-check.p.rapidapi.com/?url=https%3A%2F%2F${domain}`, options)
+				.then(response => response.json())
+				.then(response => console.log(response))
+				.catch(err => console.error(err));
+
+			const json = await response.json();
+			setData(json.isScam);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		getFishingScam(domain);
+	}, [domain]);
+
+	// JSX returns
+	return (
+		<div className='message-block'>
+			{isLoading && <div className='title'>Loading...</div>}
+			{!isLoading && (
+				<>
+					<div className="input">{domain}</div>
+					<div className="input">{data === true ? 'Scam': 'Not Scam'}</div>
+				</>
+			)}
+		</div>
+	);
+};
+
+
+// Type Constraints
+ExerraPhishing.propTypes = {
+	domain: PropTypes.string
+};
+
+
+export default ExerraPhishing;
